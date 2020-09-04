a50f81348da613742199762109a4ce6cf369ec28
varun115 <50090823+varun115@users.noreply.github.com>
2020-09-04 16:32:42 +0530

    Updates

---
+++ precache-manifest.539cab84d7f02a0c57d1bd039b15bca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8060f0c7c7d4859d475b7245db7f4601",
+    "url": "/covid-19_status/index.html"
+  },
+  {
+    "revision": "99b5bfc26feb8789eb2f",
+    "url": "/covid-19_status/static/css/main.82d91c03.chunk.css"
+  },
+  {
+    "revision": "e23133aa834474808a79",
+    "url": "/covid-19_status/static/js/2.26e82130.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19_status/static/js/2.26e82130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b5bfc26feb8789eb2f",
+    "url": "/covid-19_status/static/js/main.c19b090a.chunk.js"
+  },
+  {
+    "revision": "691414cef4b8d2fa7e60",
+    "url": "/covid-19_status/static/js/runtime-main.9c84f6a0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19_status/static/media/image.d7265326.png"
+  }
+]);
